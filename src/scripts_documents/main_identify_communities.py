7492ba590d89886e2cf3e6395a852f5abbb0d695
Daniel Muñoz Gonzalez <dani16595@gmail.com>
2026-04-23 12:29:55 +0200

    simulate differents communities

--- src/scripts_documents/main_identify_communities.py
+++ src/scripts_documents/main_identify_communities.py
@@ -1,58 +1,57 @@
-from hashlib import algorithms_available
+from community_opinions_dynamics.graph_factory import GraphFactory
+from networkx.algorithms.community import (louvain_communities, modularity,
+                                           greedy_modularity_communities, label_propagation_communities)
+from utils import compute_modularity_asyn_fluidc, compute_modularity_k_clique
+import statistics
 
-from graph_factory import GraphFactory
-from networkx.algorithms.community import (louvain_communities, modularity, k_clique_communities,
-                                           greedy_modularity_communities, label_propagation_communities,
-                                           asyn_fluidc)
-import networkx as nx
 
 algorithms = {
     "louvain": louvain_communities,
-    "k_clique": k_clique_communities,
     "greedy_modularity": greedy_modularity_communities,
     "label_propagation": label_propagation_communities,
-    "asyn_fluidc": asyn_fluidc
 }
 
-def save_graph_communities(graph, communities, path: str):
-    for community_id, nodes in enumerate(communities):
-        for node in nodes:
-            graph.nodes[node]["community"] = community_id
-
-    for node in graph.nodes():
-        graph.nodes[node]["label"] = str(node)
-
-    nx.write_gexf(graph, path)
 
 
 NODES = 1000
 EDGES = 5000
+N_SIMULATORS = 100
+STEPS = 10
 COMMUNITIES = 10
 P_INTER = [0.3, 0.9]
 
-graph_factory = GraphFactory(NODES, EDGES, COMMUNITIES, 0.3)
-graph_generated, _ = graph_factory.generate_graph()
 
-for i in range(1, 100):
-    communities = asyn_fluidc(graph_generated, k=i)
-    try:
-        mod = modularity(graph_generated, communities)
-    except:
-        pass
-    else:
-        print(mod)
+for p in P_INTER:
+    history_modularity = {algorithm: [] for algorithm in algorithms.keys()}
+    history_modularity["k_clique"] = []
+    history_modularity["asyn_fluidc"] = []
+    k_values_clique = set()
+    k_values_fluidc = set()
 
+    for i in range(N_SIMULATORS):
+        print("Step:", i, "p_inter:", p)
+        graph_factory = GraphFactory(NODES, EDGES, COMMUNITIES, p)
+        graph_generated, _ = graph_factory.generate_graph()
 
-# for p in P_INTER:
-#     graph_factory = GraphFactory(NODES, EDGES, COMMUNITIES, p)
-#     graph_generated, _ = graph_factory.generate_graph()
-#
-#     for algorithm_name, algorithm_func in algorithms.items():
-#
-#         if algorithm_name == "asyn_fluidc" or algorithm_name == "k_clique":
-#             communities_algorithm = algorithm_func(graph_generated, k=5)
-#         else:
-#             communities_algorithm = algorithm_func(graph_generated)
-#
-#         mod = modularity(graph_generated, communities_algorithm)
-#         print(f"p_inter: {p} | algorithm {algorithm_name} | Modularity: {mod:.4f}")
+        k, mod = compute_modularity_k_clique(graph=graph_generated)
+        history_modularity["k_clique"].append(mod)
+        k_values_clique.add(k)
+
+        k, mod = compute_modularity_asyn_fluidc(graph=graph_generated)
+        history_modularity["asyn_fluidc"].append(mod)
+        k_values_fluidc.add(k)
+
+        for algorithm_name, algorithm_func in algorithms.items():
+            communities_algorithm = algorithm_func(graph_generated)
+            mod = modularity(graph_generated, communities_algorithm)
+            history_modularity[algorithm_name].append(mod)
+
+    for algorithm_name in history_modularity.keys():
+        if algorithm_name == "k_clique":
+            print(f"k values: {k_values_clique}")
+        elif algorithm_name == "asyn_fluidc":
+            print(f"k values: {k_values_fluidc}")
+        mean_modularity_alg = statistics.mean(history_modularity[algorithm_name])
+        std_alg = statistics.stdev(history_modularity[algorithm_name])
+        print(f"p_inter={p:.2f}, Algorithm: {algorithm_name}, Mean Modularity: {mean_modularity_alg:.4f}, "
+              f"Std Dev: {std_alg:.4f}")
